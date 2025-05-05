23a15178769f5dc9ba69fc46851ef99cecea516b
Luis Toledo <ltoledop.dev@gmail.com>
2025-05-04 20:10:49 -0500

    UPDATE:Change config cpanel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import { componentTagger } from 'lovable-tagger';
 
 export default defineConfig(({ mode }) => ({
-  base: '/inclap/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
